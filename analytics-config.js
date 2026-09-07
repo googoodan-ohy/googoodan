b868101e9f51c15d13bb480b2813f3e746c0c4ef
googoodan-ohy <ohy0973@gmail.com>
2026-09-07 22:31:33 +0900

    Connect GA4 measurement with consent and operator exclusions

--- analytics-config.js
+++ analytics-config.js
@@ -1,6 +1,6 @@
 // Enter the public GA4 Measurement ID after creating a Web data stream.
 window.GD_ANALYTICS_CONFIG = {
-  measurementId: '',
+  measurementId: 'G-2VGQSM846S',
   allowedHosts: ['googoodan.com', 'www.googoodan.com'],
   debug: false
 };
