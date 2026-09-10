68bd33e42a563d47c1d22721be5206fc97c81e72
googoodan-ohy <ohy0973@gmail.com>
2026-09-11 00:01:58 +0900

    Add English geometry transformations

--- en/types.js
+++ en/types.js
@@ -109,6 +109,7 @@ function generate(id,seed,count=20){
 }
 for(const [id,title,bankId] of [['bar-graph','Bar Graph','2-2-5'],['picture-graph','Picture Graph','3-2-6'],['line-graph','Line Graph','4-2-5']])types.push({id,title:title+' Worksheets',family:'Data',group:'Data',example:'2 + 3',instruction:'Read the data, complete the graph and explain your answer.',bankId});
 types.push({id:'word-problems',title:'Math Word Problems Worksheets',family:'Stories',group:'Stories',example:'2 + 3',instruction:'Read carefully. Write an equation and explain your answer.',bankId:'1-2-6'});
+types.push({id:'transformations',title:'Geometry Transformations Worksheets - Slides, Flips and Turns',family:'Geometry',group:'Geometry',example:'2 + 3',instruction:'Use the grid to slide, flip or turn each shape.',bankId:'4-1-4'});
 root.Worksheets={types,generate,rational,exact,fraction};
 if(typeof module!=='undefined')module.exports=root.Worksheets;
 })(globalThis);
